a525f6e91c1d942e162f744cf2acc2d0cc3a55e8
Ozioma45 <oziomaegole@gmail.com>
2023-02-25 13:06:36 -0800

    glad i finished it, though with the help of chat gpt ai

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if (typeof num1 !== 'number' || isNaN(num1) || num1 < 1 || typeof num2 !== 'number' || isNaN(num2) || num2 < 1) {
+        return "ERROR";
+      }
+      if (num1 < num2) {
+        [num1, num2] = [num2, num1];
+      }
+      let sum = 0;
+      for (let i = num2; i <= num1; i++) {
+        sum += i;
+      }
+      return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+  
