1a895e932d6ef08d192ce612d5ec8406095c20b2
ozioma45 <oziomaegole@gmail.com>
2023-06-06 18:18:32 +0100

    added a function that plays a single round

--- RPS/script.js
+++ RPS/script.js
@@ -1,11 +1,32 @@
 console.log('hello world')
 
 //for the computer choice
-const arr = ["Rock","paper","Scissors"]
-function getComputerChioce(){
+const arr = ["Rock","Paper","Scissors"]
+function getComputerChoice(){
       var computerChoice = arr[(Math.floor(Math.random() * arr.length))]
 
       return computerChoice
 }
 
-console.log(getComputerChioce())
+function game(){}
+
+function playRound(playerSelection, computerSelection, roundtime) {
+    if(playerSelection == computerSelection){
+        return "Its a draw"
+    }else if (playerSelection=="Rock" && computerSelection=="Scissors"){
+        return "You win! Rock beats Scissors"
+    }else if(playerSelection == "Paper" && computerSelection == "Rock"){
+        return "You win! Paper beats Rock"
+    }else if(
+        playerSelection ="Scissors" && computerSelection == "Paper"
+    ){
+        return "You win! Scissors beats Paper"
+    }else{
+        return `You lose, ${computerSelection} wins`;
+    }
+  } 
+
+
+  const playerSelection = "Paper";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
