d631b9a05eb8a18b9173db89d42a710929f7a274
ozioma45 <oziomaegole@gmail.com>
2023-06-19 18:45:11 +0100

    added the random button and its functionality

--- Etch_e_sketch/script.js
+++ Etch_e_sketch/script.js
@@ -30,6 +30,22 @@ function changeColor(e) {
   e.target.style.backgroundColor = `black`; 
 }
 
+function randomColor() {
+    const gridElements = document.getElementsByClassName("grid-element");
+    for (let i = 0; i < gridElements.length; i++) {
+      gridElements[i].addEventListener("mouseover", rainbow);
+    }
+  }
+  
+
+  function rainbow(e) {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  }
+  
+
 function changeSize() {
   let newSize = prompt("Enter new size");
 
