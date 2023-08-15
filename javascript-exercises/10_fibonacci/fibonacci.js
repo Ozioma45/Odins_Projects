f06c6a8523efb149846a0e5ad8713d65aacecdb5
ozioma45 <oziomaegole@gmail.com>
2023-08-15 06:36:58 +0100

    done with the exercise

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    if (num < 0) return "OOPS";
+    
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    
+    for (let i = 2; i <= num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    
+    return num2;
 };
 
 // Do not edit below this line
