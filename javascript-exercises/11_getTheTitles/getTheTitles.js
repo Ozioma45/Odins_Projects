72a042e98c0a1eaddda0a807b6c7797ed40ce758
ozioma45 <oziomaegole@gmail.com>
2023-08-15 06:48:27 +0100

    done with this

--- javascript-exercises/11_getTheTitles/getTheTitles.js
+++ javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+ const title = book.map(books => `${books.title}`);
+ 
+ return title;
 };
 
 // Do not edit below this line
