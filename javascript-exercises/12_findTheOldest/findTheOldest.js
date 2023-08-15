64b00cc56b8d5def36dc7e4f0112f3331980b31b
ozioma45 <oziomaegole@gmail.com>
2023-08-15 07:55:45 +0100

    completed the exercise

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+    const ordered = people.map(person => {
+        if (!person.yearOfDeath) {
+            const currentYear = new Date().getFullYear();
+            return { ...person, yearOfDeath: currentYear };
+        }
+        return person;
+    }).sort((a, b) => {
+        const ageA = (a.yearOfDeath - a.yearOfBirth);
+        const ageB = (b.yearOfDeath - b.yearOfBirth);
+        return ageA - ageB;
+    });
+    
+    return ordered[ordered.length - 1];
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
