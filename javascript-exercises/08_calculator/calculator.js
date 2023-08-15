f0c6aad4b79708358b0ec462423cb77325d9d481
ozioma45 <oziomaegole@gmail.com>
2023-08-15 05:47:39 +0100

    finished the exercise

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+  let c = a + b;
+  return c; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let c = a - b;
+  return c
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+// removed initial value from reduce (no 0)
+let result = arr.reduce((sum, current) => sum + current,0);
+
+return result; 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var pro = 1;
+  for (i = 0; i < arr.length; i++)
+      pro = pro * arr[i];
+  return pro;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let c = a ** b;
+  return c
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
