0fb82b2186b1ca066e0846c718aeb0e8c3c7e321
ozioma45 <oziomaegole@gmail.com>
2023-08-15 06:03:42 +0100

    done with this exercise

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordR= word.split(" ").reverse().join('');
 
+            if (word == wordR){
+                return "true"
+            }else {
+                return "false"
+            }
 };
 
 // Do not edit below this line
