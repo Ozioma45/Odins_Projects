4f017b5f6d361bbdd7a97b71c84ce8e322742714
ozioma45 <oziomaegole@gmail.com>
2023-10-14 06:35:50 +0100

    the sliders works perfectly, and the books are added and slide smoothly

---
+++ E-Library/script2.js
@@ -0,0 +1,57 @@
+/* var swiper = new Swiper(".slide-content", {
+  loop: true, // Enable loop
+  // Other Swiper options
+  slidesPerView: 1,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+}); */
+
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 'auto',
+  centeredSlides: false,
+  loop: true, // Enable looping
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
