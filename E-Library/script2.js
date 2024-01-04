1cc301b403c1a3c034f3aa218d746b5c72ba587f
ozioma45 <oziomaegole@gmail.com>
2024-01-04 19:41:23 +0100

    created and designed the intro page

--- E-Library/script2.js
+++ E-Library/script2.js
@@ -1,38 +1,5 @@
-//HANDLES THE SWIPER
-
-
-/* var swiper = new Swiper(".slide-content", {
-  loop: true, // Enable loop
-  // Other Swiper options
-  slidesPerView: 1,
-  centeredSlides: false,
-  slidesPerGroupSkip: 1,
-  grabCursor: true,
-  keyboard: {
-    enabled: true,
-  },
-  breakpoints: {
-    769: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-}); */
-
-
 var swiper = new Swiper(".slide-content", {
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   centeredSlides: false,
   loop: false, // Enable looping
   slidesPerGroupSkip: 1,
